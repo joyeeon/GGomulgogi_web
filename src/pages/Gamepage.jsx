ef4906748321422e52aa771495b713e0f24c4c95
chy031202 <chy031202@gmail.com>
2026-08-02 11:48:02 +0900

    docs:주석 제거

--- src/pages/Gamepage.jsx
+++ src/pages/Gamepage.jsx
@@ -3,10 +3,10 @@ import Gamepad from "../components/Gamepage/Gamepad";
 const Gamepage = () => {
     return(
         <div className="page">
-        {/* <div className = "w-[50%] h-[100px] bg-red-200">   로고 대신</div>
+        <div className = "w-[50%] h-[100px] bg-red-200">   로고 대신</div>
         
         <div className = "w-[50%] h-[100px] bg-red-200">   게임 화면</div>
-        <Gamepad/> */}
+        <Gamepad/>
         </div>
     )
 }
